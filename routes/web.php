9e5b17197779ff134af997f3403f2716d4ed7d5b
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2021-05-10 22:50:35 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,50 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 // User
 
-Route::get('/user-forgot-password', function () {
-    return view('/user/forgot-password');
-});
+Route::get('/user-welcome', 'App\Http\Controllers\UserController@welcome');
 
-Route::get('/user-login', function () {
-    return view('/user/login');
-});
+Route::get('/user-login', 'App\Http\Controllers\UserController@login');
 
-Route::get('/user-new-password', function () {
-    return view('/user/new-password');
-});
+Route::get('/user-register', 'App\Http\Controllers\UserController@register');
 
-Route::get('/user-privacy-policy', function () {
-    return view('/user/privacy-policy');
-});
+Route::get('/user-user-form', 'App\Http\Controllers\UserController@user_form');
 
-Route::get('/user-qr-login', function () {
-    return view('/user/qr-login');
-});
+Route::get('/user-forgot-password', 'App\Http\Controllers\UserController@forgot_password');
 
-Route::get('/user-qr-register', function () {
-    return view('/user/qr-register');
-});
+Route::get('/user-verify-forgot-password', 'App\Http\Controllers\UserController@verify_forgot_password');
 
-Route::get('/user-register', function () {
-    return view('/user/register');
-});
+Route::get('/user-new-password', 'App\Http\Controllers\UserController@new_password');
 
-Route::get('/user-terms-condition', function () {
-    return view('/user/terms-condition');
-});
+Route::get('/user-qr-register', 'App\Http\Controllers\UserController@qr_register');
 
-Route::get('/user-form', function () {
-    return view('/user/user-form');
-});
+Route::get('/user-qr-login', 'App\Http\Controllers\UserController@qr_login');
 
-Route::get('/user-verify-forgot-password', function () {
-    return view('/user/verify-forgot-password');
-});
-Route::get('/user-welcome', function () {
-    return view('/user/welcome');
-});
+Route::get('/user-privacy-policy', 'App\Http\Controllers\UserController@privacy_policy');
+
+Route::get('/user-terms-condition', 'App\Http\Controllers\UserController@terms_condition');
 
 
 // Admin
