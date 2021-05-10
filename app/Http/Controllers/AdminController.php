1011c105f8767b53bd2f3b4dc0dca3dc22246a3a
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2021-05-10 21:27:48 +0800

    May 10 - Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Admin;
+
 
 class AdminController extends Controller
 {
-    //
+    public function welcome(Request $request)
+    {
+        return view('/admin/welcome');
+    }
+
+    public function login(Request $request)
+    {
+        return view('/admin/login');
+    }
+
+    public function receiver_qr(Request $request)
+    {
+        return view('/admin/receiver-qr');
+    }
+
+    public function table_list (Request $request)
+    {
+        return view('/admin/table-list');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Admin
+
+Route::get('/admin-welcome', 'App\Http\Controllers\AdminController@welcome');
+
+Route::get('/admin-login', 'App\Http\Controllers\AdminController@login');
+
+Route::get('/admin-table-list', 'App\Http\Controllers\AdminController@table_list');
+
+Route::get('/admin-receiver-qr', 'App\Http\Controllers\AdminController@receiver_qr');
+
+// User
