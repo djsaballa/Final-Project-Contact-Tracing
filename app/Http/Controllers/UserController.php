16f8cb7186f5d79d78c494ff5666b1cf7ce9aaaf
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2021-05-11 15:44:49 +0800

    Updated Model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ContactTracing;
 
 class UserController extends Controller
 {
--- app/Models/ContactTracing.php
+++ app/Models/ContactTracing.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class ContactTracing extends Model
 {
     use HasFactory;
 
+    protected $table = 'user_info';
     protected $table = 'user_qr';
+
 }
